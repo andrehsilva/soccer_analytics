2468f09acb6b842331cf2797013dcbcdb352c43d
andrehsilva <andrehrsilva@gmail.com>
2025-01-06 22:49:39 -0300

    sidebar do projeto

--- app.py
+++ app.py
@@ -1,2 +1,15 @@
 import streamlit as st
 
+# Exibindo a imagem na sidebar
+st.sidebar.image('https://logodownload.org/wp-content/uploads/2017/11/cbf-logo-selecao-logo-brasil.png', width=100)
+
+# Navegação entre páginas
+page = st.sidebar.selectbox("Escolha uma página:", ["Home", "Football"])
+
+# Conteúdo para cada página
+if page == "Home":
+    st.title("Página Inicial")
+    st.write("Bem-vindo à página inicial do Campeonato Brasileiro!")
+elif page == "Football":
+    st.title("Futebol")
+    st.write("Aqui está a página de Futebol!")
