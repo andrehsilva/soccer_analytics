5c12a53b7436d3de6f5b2c5869b2286195a47e3d
andrehsilva <andrehrsilva@gmail.com>
2025-01-12 20:28:43 -0300

    ajsute

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import numpy as np  # linear algebra
 import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
-from streamlit_card import card
 import os
 import requests
 
